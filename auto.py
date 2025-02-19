8dfa850e75b856e3946c6bcade49bec1ebc67fe7
unknown <parkbyunee555@gmail.com>
2025-02-19 08:03:39 +0700

    Update auto.py

--- auto.py
+++ auto.py
@@ -52,8 +52,8 @@ def model(feature , target_column):
     
     # Create an AutoSklearn classifier
     classifier = autosklearn.classification.AutoSklearnClassifier(
-        time_left_for_this_task=1800,  # 30 minutes (1800 seconds)
-        per_run_time_limit=300        # 5 minutes per model training (adjust as needed)
+        time_left_for_this_task=900,  # 30 minutes (1800 seconds)
+        per_run_time_limit=100        # 5 minutes per model training (adjust as needed)
         ) 
         #time_left_for_this_task=30, per_run_time_limit=10
 
